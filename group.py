0f3be47696606a9011ce9671e2ece1c80ad76139
Matthew Sorsby <matthew.anti@gmail.com>
2018-09-12 10:53:55 +0100

    Add docstring to new class.

--- group.py
+++ group.py
@@ -1,5 +1,12 @@
 class Group:
+"""
+Represents a group ladder/leaderboard.
 
+Arguments:
+name - name of the group leaderboard.
+ladder - instance of Ladder which stores leaderboard information for group.
+"""
+    
     def __init__(self, name, ladder):
         self.name = name
         self.ladder = ladder
