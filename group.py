a346c62bdd1a8c921334ac1e2bfaa4587ea266d1
Matthew Sorsby <matthew.anti@gmail.com>
2018-09-12 10:54:51 +0100

    Fix indent.

--- group.py
+++ group.py
@@ -1,12 +1,15 @@
 class Group:
-"""
-Represents a group ladder/leaderboard.
 
-Arguments:
-name - name of the group leaderboard.
-ladder - instance of Ladder which stores leaderboard information for group.
-"""
-    
-    def __init__(self, name, ladder):
+
+    '''
+    Represents a group ladder/leaderboard.
+
+    Arguments:
+    name - name of the group leaderboard.
+    ladder - instance of Ladder which stores leaderboard information for group.
+    '''
+
+
+def __init__(self, name, ladder):
         self.name = name
         self.ladder = ladder
