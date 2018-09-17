0f8f10b4fd6be4926500c3e5a79a30d9093bc48d
Matthew Sorsby <matthew.anti@gmail.com>
2018-09-17 14:01:12 +0100

    Add testing directory structure.

---
+++ test/test_update_ladder_standings.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestUpdateLadderStandings(unittest.TestCase):
+
+    def test_both_players_in_ladder_winner_higher_rank(self):
+        pass
+
+    def test_both_players_in_ladder_winner_lower_rank(self):
+        pass
+
+    def test_winner_in_ladder_loser_not(self):
+        pass
+
+    def test_loser_in_ladder_winner_not(self):
+        pass
+
+    def test_both_not_in_ladder(self):
+        pass
