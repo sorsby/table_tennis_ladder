d1da776055f7429c875b084374124a1c35bb4ec8
Matthew Sorsby <matthew.anti@gmail.com>
2018-09-17 16:35:21 +0100

    Add remove player test class.

--- test/test_remove_player.py
+++ test/test_remove_player.py
@@ -1,11 +1,21 @@
 import unittest
-from ladder_mock import LadderMock
-
+from ladder import Ladder
+from player import Player
 
 class TestRemovePlayer(unittest.TestCase):
-    
+
+    ladder = Ladder('test_group')
+
+    def tearDown(self):
+        self.ladder.clear()
+
     def test_remove_player_in_ladder(self):
-        pass
+        self.ladder.add_player('p1')
+        self.ladder.add_player('p2')
+        self.ladder.remove_player('p1')
+
+        expected = [self.ladder.get_player('p2')]
+        self.assertListEqual(expected, self.ladder.ladder)
 
 if __name__ == '__main__':
     unittest.main()
